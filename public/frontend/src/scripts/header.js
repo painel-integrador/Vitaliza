e7e94546f35c2afa60608c81b5c86cf060b35f66
timoty33 <timotyduo@gmail.com>
2026-09-15 21:52:42 -0300

    Terminado JS e HTML do header, falta apenas o CSS

--- public/frontend/src/scripts/header.js
+++ public/frontend/src/scripts/header.js
@@ -1,10 +1,10 @@
 let isMenuAberto = false;
+let botaoMenu = document.getElementById("botao-menu");
+let botaoFechar = document.getElementById("botao-fechar");
+let nav = document.getElementById("nav");
+let btns = document.getElementById("btns");
 
 function menu() {
-  let botaoMenu = document.getElementById("botao-menu");
-  let botaoFechar = document.getElementById("botao-fechar");
-  let nav = document.getElementById("nav");
-
   // Menu fechado vai abrir
 
   if (!isMenuAberto) {
@@ -24,4 +24,20 @@ function menu() {
   }
 
   isMenuAberto = !isMenuAberto; // troca o valor boolean (true => false | false => true)
+}
+
+// Menu com conta logada
+
+if (document.cookie.includes("refresh_token=")) {
+  // esconder os botoes
+  btns.querySelectorAll("a").forEach((element) => {
+    element.classList.add("invisivel");
+  });
+
+  console.table(btns);
+
+  // mostrar itens do nav
+  nav.querySelectorAll(".invisivel").forEach((element) => {
+    element.classList.remove("invisivel");
+  });
 }
