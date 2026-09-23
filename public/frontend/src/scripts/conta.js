33d92fa77fbc7fd41bd1d09226065a1ce5035afa
andrematthies <andreluizmatthies@gmail.com>
2026-09-23 14:41:00 -0300

    confirmação login

--- public/frontend/src/scripts/conta.js
+++ public/frontend/src/scripts/conta.js
@@ -26,3 +26,32 @@ async function criarConta(event) {
     console.error("Erro ao processar criação de conta:", e);
   }
 }
+
+async function login(event) {
+  event.preventDefault();
+
+  const form = document.getElementById("formulario");
+  const formData = new FormData(form);
+  const dados = Object.fromEntries(formData);
+
+  try {
+    const response = await fetch("/api/auth/login/email_senha", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dados),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+
+    // Se o backend respondeu com redirect (302), a propriedade .url conterá o destino final
+    if (response.redirected) {
+      window.location.href = response.url;
+    }
+  } catch (e) {
+    console.error("Erro ao processar login:", e);
+  }
+}
