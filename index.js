da9ca82d06d9c479c9dc3103ff1635c32cf0c4cc
timoty33 <timotyduo@gmail.com>
2026-09-25 15:12:04 -0300

    Adicionado servidor rodando em producao

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import "dotenv/config";
 import servidor from "./src/servidor/servidor.js";
 
-servidor.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    servidor.log.error(err);
-    process.exit(1);
-  }
-});
+if (process.env.ENV === "desenvolvimento") {
+  servidor.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+      servidor.log.error(err);
+      process.exit(1);
+    }
+  });
+} else if (process.env.ENV === "producao") {
+  servidor.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
+    if (err) {
+      servidor.log.error(err);
+      process.exit(1);
+    }
+  });
+}
